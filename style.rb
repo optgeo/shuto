c8a85eeabad7dea79a17a5c214b2f4ca7a913376
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2021-10-04 07:16:09 +0900

    update

--- style.rb
+++ style.rb
@@ -26,11 +26,72 @@ maxzoom: 13
 EOS
 )
 
+style['sources']['v'] = YAML.load(
+<<EOS
+type: vector
+tiles:
+  - https://optgeo.github.io/unite-one/zxy/{z}/{x}/{y}.pbf
+minzoom: 10
+maxzoom: 12
+attribution: 国土地理院ベクトルタイル提供実験
+EOS
+)
+
 style['terrain'] = YAML.load(
 <<EOS
 source: h
 EOS
 )
+
+style['layers'].unshift(YAML.load(
+<<EOS
+id: one
+type: fill
+source: v
+source-layer: one
+paint: 
+  fill-color: 
+    - match
+    -
+      - get
+      - code
+    - 山地
+    - "#d9cbae"
+    - 崖・段丘崖
+    - "#9466ab"
+    - 地すべり地形
+    - "#cc99ff"
+    - 台地・段丘
+    - "#ffaa00"
+    - 山麓堆積地形
+    - "#99804d"
+    - 扇状地
+    - "#cacc60"
+    - 自然堤防
+    - "#ffff33"
+    - 天井川
+    - "#fbe09d"
+    - 砂州・砂丘
+    - "#ffff99"
+    - 凹地・浅い谷
+    - "#a3cc7e"
+    - 氾濫平野
+    - "#bbff99"
+    - 後背低地・湿地
+    - "#00d1a4"
+    - 旧河道
+    - "#6699ff"
+    - 落堀
+    - "#1f9999"
+    - 河川敷・浜
+    - "#9f9fc4"
+    - 水部
+    - "#e5ffff"
+    - 旧水部
+    - "#779999"
+    - "#f00"
+EOS
+))
 
 style['layers'].unshift(YAML.load(
 <<EOS
